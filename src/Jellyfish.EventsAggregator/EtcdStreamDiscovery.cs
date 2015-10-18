a1646eaae541c8195d541ba08743fe53594d4873
Alain Metge <alain@macalain.home>
2015-10-18 14:39:41 +0200

    update license

--- src/Jellyfish.EventsAggregator/EtcdStreamDiscovery.cs
+++ src/Jellyfish.EventsAggregator/EtcdStreamDiscovery.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//    Copyright (c) Zenasoft
+//
+using System;
 using System.Linq;
 using System.Reactive.Linq;
 using Draft;
 using Draft.Responses;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Reactive.Disposables;
-using Newtonsoft.Json.Linq;
 using System.Threading;
 
 namespace Jellyfish.EventsAggregator
@@ -20,7 +32,7 @@ namespace Jellyfish.EventsAggregator
 
         public EtcdStreamDiscovery()
         {
-            _etcd = Etcd.ClientFor(new Uri("http://192.168.1.44:2379"));
+            _etcd = Etcd.ClientFor(new Uri("http://local-etcd:2379"));
 #if DEBUG
             _key = "/jellyfish/runtime/test/services";
 #else
